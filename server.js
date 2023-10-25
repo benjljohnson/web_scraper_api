19bea7cd13e6b69186727b1014f6bddcc4188663
benjljohnson <149022439+benjljohnson@users.noreply.github.com>
2023-10-25 17:35:32 -0600

    Update server.js
        git commit --amend --reset-author
    
     1 file changed, 1 insertion(+), 1 deletion(-)

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const requestPromise = require('request-promise');
 const cheerio = require('cheerio');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
